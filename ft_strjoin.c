705f64ee1a78805a9cfade0f8f5ef426322d64fb
L'haiba <lhaiba@Lhaibas-MacBook-Pro.local>
2024-01-07 03:32:58 +0100

    ft_substr and ft_strjoin added

---
+++ ft_strjoin.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* ft_strjoin(const char* s1, const char* s2) {
+    if (!s1 || !s2)
+        return NULL;
+
+    size_t len_s1 = strlen(s1);
+    size_t len_s2 = strlen(s2);
+
+    char* total = (char*)malloc((len_s1 + len_s2 + 1) * sizeof(char));
+
+    if (!total)
+        return NULL;
+
+    size_t i = 0;
+    while (i < len_s1) {
+        total[i++] = *s1++;
+    }
+
+    while (i < (len_s1 + len_s2)) {
+        total[i++] = *s2++;
+    }
+
+    total[i] = '\0';
+    return total;
+}
+
+int main() {
+    const char* s1 = "whsup ";
+    const char* s2 = "bruh!";
+
+    char* result = ft_strjoin(s1, s2);
+    if (result != NULL) {
+        printf("The full string is: %s\n", result);
+        free(result);
+    } else {
+        printf("memory allocation failed or wrong input: Error\n");
+    }
+
+    return 0;
+}
---
+++ ft_substr.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <string.h>
+
+char* ft_strncpy(char* destination, const char* source, size_t num) 
+{
+    if (destination == NULL || source == NULL || num == 0) 
+    {
+        return destination;
+    }
+
+    char* dest_ptr = destination;
+
+    while (*source != '\0' && num > 0) 
+    {
+        *dest_ptr = *source;
+        dest_ptr++;
+        source++;
+        num--;
+    }
+
+    while (num > 0) 
+    {
+        *dest_ptr = '\0';
+        dest_ptr++;
+        num--;
+    }
+
+    return destination;
+}
+
+char* ft_substr(const char* str, int start, int len)
+{
+    if (str == NULL || start < 0 || len < 0) {
+        return NULL;
+    }
+
+    int str_len = ft_strlen(str);
+    if (start >= str_len) {
+        return NULL;
+    }
+
+    int sec_len = (start + len > str_len) ? (str_len - start) : len;
+    char* substring = (char*)malloc((sec_len + 1) * sizeof(char));
+    if (substring == NULL) {
+        return NULL;
+    }
+
+    ft_strncpy(substring, str + start, sec_len);
+    substring[sec_len] = '\0';
+    return substring;
+}
+
+int main()
+{
+    const char* original = "Amine el haiba";
+
+    char* result = ft_substr(original, -3, 8);
+    if (result != NULL) {
+        printf("Substring is :%s\n", result);
+        free(result);
+    } else {
+        printf("wrong input : Error\n");
+    }
+
+    return 0;
+}
