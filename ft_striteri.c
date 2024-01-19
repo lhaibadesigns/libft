e0389051bcd3d0b420e400f195edaeac30c873f4
Amine El Haiba <ael-haib@c2r6s2.42madrid.com>
2024-01-19 17:21:57 +0100

    ft_striteri is done

---
+++ ft_striteri.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-haib <ael-haib@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 15:13:36 by ael-haib          #+#    #+#             */
+/*   Updated: 2024/01/19 17:18:12 by ael-haib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void ft( unsigned int i, char *c)
+{
+  *c = 'a';
+}
+
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+    unsigned int i;
+
+    i = 0;
+    while (s[i])
+    {
+        ft(i , &s[i]);
+        i++;
+    }
+}
+
+int main()
+{
+    int fd = open("file.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    printf("fd: %d\n", fd);
+
+    return 0;
+    
+    char s[] = "Amine";
+    ft_striteri(s, ft);
+    printf("s = :%s", s);
+    return (0);
+}
