3a5936d53186ceafc1feecc2bcd28863bf88689c
Amine El Haiba <ael-haib@c2r9s1.42madrid.com>
2024-01-20 20:53:30 +0100

    putstr_fd is done

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,36 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ael-haib <ael-haib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/20 18:10:19 by ael-haib          #+#    #+#             */
-/*   Updated: 2024/01/20 18:58:54 by ael-haib         ###   ########.fr       */
+/*   Created: 2024/01/20 18:50:08 by ael-haib          #+#    #+#             */
+/*   Updated: 2024/01/20 20:52:06 by ael-haib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
 
-void ft_putstr_fd(char *s, int fd)
+void ft_putchar_fd(char c, int fd)
 {
-    
-    while(*s)
+    write(fd, &c, 1);
+}
+
+void ft_putnbr_fd(int n, int fd)
+{
+    if (n < 0)
     {
-        write(fd, s, 1);
-        s++;
+        write(fd, "-", 1);
+        n *= -1;
     }
-    write(fd, "\n", 1);
+
+    if (n <= 9 && n >= 0)
+     ft_putchar_fd((n + 48), fd);
+
+    if (n > 9)
+    {
+        ft_putnbr_fd((n / 10), fd);
+        ft_putchar_fd(((n % 10) + '0'), fd);
+    }
 }
 
 int main()
 {
+    int n = -456789;
     int fd;
-
-    char s[] = "Amineelhaiba";
     fd = open("test.txt", O_WRONLY | O_CREAT, 0777);
 
-    ft_putstr_fd(s, fd);
+    ft_putnbr_fd(n , fd);
     return(0);
 }
