e0a7d3949ab61494de5706555620d7065bcdfa8f
Amine El Haiba <ael-haib@c2r9s1.42madrid.com>
2024-01-20 20:54:47 +0100

    putstr_fd is done

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: ael-haib <ael-haib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 18:10:19 by ael-haib          #+#    #+#             */
-/*   Updated: 2024/01/20 18:58:54 by ael-haib         ###   ########.fr       */
+/*   Updated: 2024/01/20 20:54:38 by ael-haib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int main()
 {
     int fd;
 
-    char s[] = "Amineelhaiba";
+    char s[] = "Amineelhai";
     fd = open("test.txt", O_WRONLY | O_CREAT, 0777);
 
     ft_putstr_fd(s, fd);
